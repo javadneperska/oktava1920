96a9b1e597838e73751eb2174cd97fd64746778c
javadneperska <javadneperska@gmail.com>
2019-10-16 09:04:27 +0200

    Create majo-parne.java

---
+++ SiestaHodina/majo-parne.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+
+        System.out.println("Zadajte 1. cislo: ");
+            int z = sc.nextInt();
+        System.out.println("Zadajte 2. cislo: ");
+            int k = sc.nextInt();
+
+        if(z<k) {
+            for (int i=z; i<=k ; i++){
+
+
+                if(i%2==0){
+                    System.out.println(i);
+                }
+            }
+
+
+        }
+        else{
+            for(int i = k; i<=z ; i++){
+
+
+                if(i%2==0){
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
