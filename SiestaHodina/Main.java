72e1822c0ca4382149a30dc042237cc9a7103912
javadneperska <javadneperska@gmail.com>
2019-10-16 09:07:08 +0200

    Add files via upload

---
+++ SiestaHodina/Main.java
@@ -0,0 +1,31 @@
+package sk.dneperska;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("hodnota 1:");
+        int A = sc.nextInt();
+        System.out.println("hodnota 2:");
+        int B = sc.nextInt();
+        if(A < B) {
+            for (int i = A; i < B + 1; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+        else {
+            for (int i = B; i < A + 1; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
+
+            }
+        }
+    }
+}
