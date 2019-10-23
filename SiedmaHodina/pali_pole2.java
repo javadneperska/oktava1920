b19c9cc12902747dce416312075d8086cc7b990b
javadneperska <javadneperska@gmail.com>
2019-10-23 10:02:25 +0200

    Create pali_pole2.java

---
+++ SiedmaHodina/pali_pole2.java
@@ -0,0 +1,11 @@
+private static void fhodina7_5()
+    {
+        int[] list = new int[5];
+        for(int i =0;i<5;i++)
+        {
+            System.out.println("zadaj cislo: ");
+            list[i] = sc.nextInt()+1;
+        }
+        System.out.println(Arrays.toString(list));
+
+    }
