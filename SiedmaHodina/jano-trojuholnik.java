aa02e45d5c67fe898f7c060b83dd746de83c5ad4
javadneperska <javadneperska@gmail.com>
2019-10-23 09:30:49 +0200

    Create jano-trojuholnik.java

---
+++ SiedmaHodina/jano-trojuholnik.java
@@ -0,0 +1,20 @@
+package sk.gymbosak;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadaj stranu:");
+        int n = sc.nextInt();
+        for (int i=0;i<n;i++)
+        {
+            for(int j = 0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
