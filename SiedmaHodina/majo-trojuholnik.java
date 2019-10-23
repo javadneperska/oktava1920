c9b2cc669f8e109d60c15bb534bc513a3bf776ab
javadneperska <javadneperska@gmail.com>
2019-10-23 10:03:47 +0200

    Create majo-trojuholnik.java

---
+++ SiedmaHodina/majo-trojuholnik.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Zadaj velkost");
+	        int n=sc.nextInt();
+
+	    for(int i = 0; i<n; i++)
+	    {
+	        for (int j = 0; j<i;j++)
+	        {
+                System.out.print("*");
+            }
+            System.out.println();
+	    }
+    }
+}
