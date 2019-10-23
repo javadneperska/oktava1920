e63be32b00bfa4c94e417c694f8f0231e53d9927
javadneperska <javadneperska@gmail.com>
2019-10-23 09:26:38 +0200

    Create pali_obdlznik.java

---
+++ SiedmaHodina/pali_obdlznik.java
@@ -0,0 +1,9 @@
+public static void main()
+{
+fhodina7_4(5,4);
+}
+private static void fhodina7_4(int a,int b)
+    {
+        System.out.println("Obsah: " + (a*b));
+        System.out.println("Obvod: " + (2*(a+b)));
+    }
