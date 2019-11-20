5cca456654c3200e4b6ee52251667ce0765a6c61
javadneperska <javadneperska@gmail.com>
2019-11-20 09:33:39 +0100

    Loading width/height

--- Projekty/Piskvorky/jano.s
+++ Projekty/Piskvorky/jano.s
@@ -1,1 +1,63 @@
-mov eax, ecx
+;x86 bootloader, write to first sector of bootable media
+bits 16
+org 0x7c00
+
+  mov sp, 1000h
+  mov bx, write_width
+write_enter:
+  mov si, str_enter
+  mov ah, 0Eh
+print:
+  lodsb
+  or al,al
+  jz jump_bx
+  int 10h
+  jmp print
+jump_bx:
+  push bx
+  ret
+write_width:
+  mov si, str_width
+  mov bx, read_width
+  jmp print
+read_width:
+  mov si, 2000h
+  mov bx, width_end
+read_char:
+  mov ah, 0h
+  int 16h
+  cmp al, 0h
+  jg char_end
+  jmp read_char
+char_end:
+  cmp al, 31h
+  jl read_char
+  cmp al, 39h
+  jg read_char
+  sub al, 30h
+  cmp al, 1
+  jne done
+  add al, 9
+done:
+  mov [si], al
+  jmp jump_bx
+width_end:
+  mov bx, write_height
+  jmp write_enter
+write_height:
+  mov si, str_height
+  mov bx, read_height
+  jmp print
+read_height:
+  mov si, 2002h
+  mov bx, height_end
+  jmp read_char
+height_end:
+  hlt
+  jmp $
+
+str_enter: db "Enter ", 0
+str_width: db "width (3-10)", 0xA, 0xD, 0
+str_height: db "height (3-10)", 0xA, 0xD, 0
+times 510-($-$$) db 0
+dw 0xAA55
