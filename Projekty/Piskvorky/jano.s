7fadeaabee433f061b04945ac89e61f42eff2e7e
javadneperska <javadneperska@gmail.com>
2019-11-20 10:25:34 +0100

    Map drawing done, started work on game loop

--- Projekty/Piskvorky/jano.s
+++ Projekty/Piskvorky/jano.s
@@ -61,19 +61,75 @@ write_len:
   jmp print
 read_len:
   mov si, 2004h
-  mov bx, len_end
+  mov bx, draw_map
   jmp read_char
-len_end:
+draw_map:
+  xor dx, dx
+  mov ch, [2000h]
+  mov cl, [2002h]
+  mov di, 2006h
+  mov ah, 0Eh
+map_start:
+  mov al, [di]
+  mov bx, map_done
+  cmp al, 1
+  jg map_x
+  jl map_n
+  mov si, str_o
+  jmp print
+map_x:
+  mov si, str_x
+  jmp print
+map_n:
+  mov si, str_n
+  jmp print
+map_done:
+  inc di
+  inc dh
+  cmp dh, ch
+  jge map_next_y
+  jmp map_start
+map_next_y:
+  xor dh,dh
+  inc dl
+  cmp dl, cl
+  jge map_end
+  mov bx, map_start
+  mov si, str_end
+  jmp print
+map_end:
+  mov bx, game_loop
+  mov si, str_end
+  jmp print
+game_loop:
+  mov bx, game_loop_xdone
+game_loop_read:
+  mov ah, 0h
+  int 16h
+  cmp al, 0h
+  jg game_loop2
+  jmp game_loop_read
+game_loop2:
+  cmp al, 30h
+  jl game_loop_read
+  cmp al, 39h
+  jg game_loop_read
+  sub al, 30h
+  jmp jump_bx
+game_loop_xdone:
+
+  jmp draw_map
+exit:
   mov ah, 0Eh
   mov al, 57h
   int 10h
   hlt
   jmp $
 
-str_enter: db "Enter ", 0
-str_width: db "width (3-10)", 0xA, 0xD, 0
-str_height: db "height (3-10)", 0xA, 0xD, 0
-str_len: db "winning length", 0xA, 0xD, 0
+str_enter: db "Zadaj ", 0
+str_width: db "sirku", 0xA, 0xD, 0
+str_height: db "vysku", 0xA, 0xD, 0
+str_len: db "vyhernu dlzku", 0xA, 0xD, 0
 str_x: db "|X", 0
 str_o: db "|O", 0
 str_n: db "| ", 0
