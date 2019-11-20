be30122a7dc2f45e647e86d530e66cc5a6b1395f
javadneperska <javadneperska@gmail.com>
2019-11-20 10:23:32 +0100

    Create pali_covert.java

---
+++ DeviataHodina/pali_covert.java
@@ -0,0 +1,9 @@
+private static void fhodina9_3_ConvertCtoF()
+    {
+        float cels = sc.nextFloat();
+        float d = 9.0f/5.0f ;
+        //System.out.println(d);
+        float f = (cels *d);
+        f+=32;
+        System.out.println(+cels + " celzia je vo farenhaitoch  "+f);
+    }
