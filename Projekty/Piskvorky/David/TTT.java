e6741e33362cf25a4d834ccc3fff2a6c2a6cc79a
javadneperska <javadneperska@gmail.com>
2019-11-20 10:57:08 +0100

    Update TTT.java

--- Projekty/Piskvorky/David/TTT.java
+++ Projekty/Piskvorky/David/TTT.java
@@ -161,17 +161,6 @@ public class TTT {
         return false;
     }
 
-    private boolean checkRowCol(char c1, char c2, char c3)
-    {
-        if(c1 != '-' && c2 != '-' && c3 != '-'){
-            if(c1 == c2 && c2 == c3){
-                return true;
-            }
-            else return false;
-        }
-        else return false;
-    }
-
     public void changePlayer() {
         if(currentPlayerMark == 'x') {
             currentPlayerMark = 'o';
