86b889c39cacdf6aba402390fa1786500a12ff82
javadneperska <javadneperska@gmail.com>
2020-04-29 09:38:10 +0200

    Create majo_3.java

---
+++ DesiataHodina/majo_3.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] pole = new int[10];
+
+        for(int i=0; i<10; i++) {
+            System.out.println("zadajte cislo");
+            pole[i] = sc.nextInt();
+        }
+        for(int i=0; i<10;i++){
+            if(pole[i]%3 == 0 || pole[i]%4 == 0){
+                System.out.println(pole[i]);
+            }
+        }
+    }
+}
